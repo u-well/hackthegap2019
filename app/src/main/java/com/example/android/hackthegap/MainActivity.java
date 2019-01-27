e59e0d3e4f79205be55bd20022b9f79f22c1986f
Carla Brady <carlakaybrady@gmail.com>
2019-01-26 18:59:52 -0600

    send notifications

--- app/src/main/java/com/example/android/hackthegap/MainActivity.java
+++ app/src/main/java/com/example/android/hackthegap/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.android.hackthegap;
 
+
+import android.app.Notification;
+import android.app.Notification.Builder;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -20,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        createNotificationChannel();
 
         Button helpButton = (Button) findViewById(R.id.help);
         helpButton.setOnClickListener(new View.OnClickListener() {
@@ -38,10 +48,60 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.i("clicks", "Go to a new page");
                 Intent i = new Intent(MainActivity.this, RelaxActivity.class);
-                startActivity(i);
             }
         });
 
+        Button smsbutton = (Button) findViewById(R.id.smsbutton);
+        smsbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("clicks", "Go to a new page");
+                Intent i = new Intent(MainActivity.this, HelpActivity.class);
+                startActivity(i);
 
+            }
+        });
+
+        Button notifyButton = findViewById(R.id.notify);
+        notifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("clicks", "Send notification");
+                addNotification();
+            }
+        });
+    }
+
+    private void addNotification() {
+
+        String CHANNEL_ID = "whenIneedU";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("whenIneedU")
+                .setContentText("In a crowded space? Try closing your eyes and counting to 10. You've got this!");
+
+        Intent notificationIntent = new Intent(MainActivity.this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(MainActivity.this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1, builder.build());
+    }
+
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "whenIneedU";
+            String description = "whenIneedU";
+            String CHANNEL_ID = "whenIneedU";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
